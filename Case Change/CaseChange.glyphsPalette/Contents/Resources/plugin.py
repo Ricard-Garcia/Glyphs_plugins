443dcab0d7bc57f39919bc0d9a61891152f694f1
Ricard Garcia <imricardgarcia@gmail.com>
2019-05-30 19:39:09 +0200

    Initial commit
    Pushed plugin and general information.

---
+++ Case Change/CaseChange.glyphsPalette/Contents/Resources/plugin.py
@@ -0,0 +1,59 @@
+# encoding: utf-8
+
+###########################################################################################################
+#
+#
+#	Palette Plugin
+#
+#	Read the docs:
+#	https://github.com/schriftgestalt/GlyphsSDK/tree/master/Python%20Templates/Palette
+#
+#
+###########################################################################################################
+
+from GlyphsApp.plugins import *
+from vanilla import *
+
+
+class ChangeCase(PalettePlugin):
+
+	def settings(self):
+
+		self.name = "Change Case"
+
+		# Create Vanilla window and group with controls
+		width = 150
+		height = 90
+		self.paletteView = Window((width, height))
+		self.paletteView.group = Group((0, 0, width, height))
+		self.paletteView.group.UCButton = Button((10, -85, width-7, 20), "Uppercase", callback=self.upperCallback)
+		self.paletteView.group.lcButton = Button((10, -60, width-7, 20), "Lowercase", callback=self.lowerCallback)
+		self.paletteView.group.titleButton = Button((10, -35, width-7, 20), "Title", callback=self.titleCallback)
+		
+		# Set dialog to NSView
+		self.dialog = self.paletteView.group.getNSView()
+
+			
+	# Uppercase
+	def upperCallback(self, sender):
+		windowController = self.windowController()
+		if windowController:
+			thisFont = windowController.document().font
+			thisFont.currentText = thisFont.currentText.upper()
+		
+	# Lowercase
+	def lowerCallback(self, sender):
+		windowController = self.windowController()
+		if windowController:
+			thisFont = windowController.document().font
+			thisFont.currentText = thisFont.currentText.lower()
+	
+	# Title
+	def titleCallback(self, sender):
+		windowController = self.windowController()
+		if windowController:
+			thisFont = windowController.document().font
+			thisFont.currentText = thisFont.currentText.title()
+
+
+
---
+++ Case Change/README.md
@@ -0,0 +1,6 @@
+# Change Case
+
+A plugin for Glyphs App that adds a palette window with three buttons in order to change the case of Edit View's text.
+
+
+![Screenshot](Image/ChangeCase.png "Change Case Screenshot")
