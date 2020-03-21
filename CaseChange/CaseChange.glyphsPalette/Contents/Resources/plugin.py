5d66ff04fc7dec33db5f174783a31c5e68d42b5b
schriftgestalt <georg.seifert@mac.com>
2020-03-21 10:22:29 +0100

    prepare for new python version

--- CaseChange/CaseChange.glyphsPalette/Contents/Resources/plugin.py
+++ CaseChange/CaseChange.glyphsPalette/Contents/Resources/plugin.py
@@ -26,18 +26,17 @@ class ChangeCase(PalettePlugin):
 		height = 90
 		self.paletteView = Window((width, height))
 		self.paletteView.group = Group((0, 0, width, height))
-		self.paletteView.group.UCButton = Button((10, -85, width-7, 20), "Uppercase", callback=self.changeCaseCallback)
+		self.paletteView.group.UCButton = Button((10, -85, width-7, 20), "Uppercase", callback=self.changeCaseCallback_)
 		self.paletteView.group.UCButton.getNSButton().setToolTip_(u"ABC Abc abc → ABC ABC ABC")
-		self.paletteView.group.lcButton = Button((10, -60, width-7, 20), "Lowercase", callback=self.changeCaseCallback)
+		self.paletteView.group.lcButton = Button((10, -60, width-7, 20), "Lowercase", callback=self.changeCaseCallback_)
 		self.paletteView.group.lcButton.getNSButton().setToolTip_(u"ABC Abc abc → abc abc abc")
-		self.paletteView.group.titleButton = Button((10, -35, width-7, 20), "Title", callback=self.changeCaseCallback)
+		self.paletteView.group.titleButton = Button((10, -35, width-7, 20), "Title", callback=self.changeCaseCallback_)
 		self.paletteView.group.titleButton.getNSButton().setToolTip_(u"ABC Abc abc → Abc Abc Abc")
-		
+
 		# Set dialog to NSView
 		self.dialog = self.paletteView.group.getNSView()
 	
-	def changeCaseCallback(self, sender):
-
+	def changeCaseCallback_(self, sender):
 		Glyphs.clearLog()
 
 	
